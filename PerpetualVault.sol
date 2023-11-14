544aff646bc6b248956df41b5dbdd01770a057df
anjanayraina <anjanayraina326@gmail.com>
2023-11-14 10:57:44 +0530

    ERC20 added

--- PerpetualVault.sol
+++ PerpetualVault.sol
@@ -1,6 +1,26 @@
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
-pragma solidity 0.8.21;
+pragma solidity 0.8.7;
 
-contract PerpetualVault is ERC4626 {
+import "https://github.com/Rari-Capital/solmate/blob/main/src/tokens/ERC20.sol";
+
+contract PerpetualVault is   ERC20 {
+    ERC20 public vaultToken;
+    constructor(address tokenAddress , string memory _name , string memory _symbol) ERC20(_name , _symbol , 18){
+        vaultToken = ERC20(tokenAddress);
+    }
+    
+    function transferFrom(address from , address to , uint amount) override public returns (bool){
+
+    }
+    function approve(address spender , uint amount) override public  returns(bool){
+
+    }
+    function balanceOf(address account) external view returns(uint256){
+
+    }
+    function transfer(address to , uint amount) external returns(bool){
+
+    }
+
+
     
 }
