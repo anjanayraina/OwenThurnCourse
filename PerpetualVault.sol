cda3b9012a861c35464393e054c8cde1f82068ed
anjanayraina <anjanayraina326@gmail.com>
2023-11-14 11:22:34 +0530

    Position added

--- PerpetualVault.sol
+++ PerpetualVault.sol
@@ -1,9 +1,16 @@
 pragma solidity 0.8.7;
 
 import "https://github.com/Rari-Capital/solmate/blob/main/src/tokens/ERC20.sol";
-
-contract PerpetualVault is   ERC20 {
+// testnet oracle : https://sepolia.etherscan.io/address/0x6090149792dAAeE9D1D568c9f9a6F6B46AA29eFD
+contract PerpetualVault is ERC20 {
     ERC20 public vaultToken;
+    
+    struct Position {
+        address creator;
+        uint collateral;
+        bool long;
+        uint size;
+    }
     constructor(address tokenAddress , string memory _name , string memory _symbol) ERC20(_name , _symbol , 18){
         vaultToken = ERC20(tokenAddress);
     }
