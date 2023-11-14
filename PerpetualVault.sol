78397516fae1db7bb7d10b874e26aa6b9a036967
anjanayraina <anjanayraina326@gmail.com>
2023-11-14 11:38:56 +0530

    Test oracle link added

--- PerpetualVault.sol
+++ PerpetualVault.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.7;
 import "https://github.com/Rari-Capital/solmate/blob/main/src/tokens/ERC20.sol";
 // testnet oracle : https://sepolia.etherscan.io/address/0x6090149792dAAeE9D1D568c9f9a6F6B46AA29eFD
 contract PerpetualVault is ERC20 {
-    ERC20 public vaultToken;
+    ERC20 public LPToken;
     
     struct Position {
         address creator;
@@ -12,7 +12,7 @@ contract PerpetualVault is ERC20 {
         uint size;
     }
     constructor(address tokenAddress , string memory _name , string memory _symbol) ERC20(_name , _symbol , 18){
-        vaultToken = ERC20(tokenAddress);
+        LPToken = ERC20(tokenAddress);
     }
     
     function transferFrom(address from , address to , uint amount) override public returns (bool){
